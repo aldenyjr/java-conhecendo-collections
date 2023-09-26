3a5a084ce36e0de8069d2882b265d93cdcaca437
Aldeny Jr <juniorfilhobom@gmail.com>
2023-09-26 20:37:02 -0300

    Aplicando exercicio HashSet Contato com AgendaContatos
    Signed-off-by: Aldeny Jr <juniorfilhobom@gmail.com>

---
+++ src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,68 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+
+    }
+
+    public void exibirContato(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+
+        for (Contato c : contatoSet){
+            if(c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("Aldeny Menezes", 12345678);
+        agendaContatos.adicionarContato("Aldeny Menezes", 1111111);
+        agendaContatos.adicionarContato("Aldeny Junior", 88888888);
+        agendaContatos.adicionarContato("Maria Passos", 6666666);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Aldeny"));
+        System.out.println("Contato Atualizado: " + agendaContatos.atualizarNumeroContato("Maria Passos", 77777777));
+
+        agendaContatos.exibirContato();
+
+
+
+
+
+
+    }
+}
